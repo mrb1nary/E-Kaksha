5d0b15154337b89c0ec091d7a142dd1e0b7e6ef5
Anubhab <anubhab.techie@gmail.com>
2022-03-21 12:00:15 +0530

    Added firebase support

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5vpBGBagcRTQ2xjXl_4FGcu2CVig20KM",
+    authDomain: "project-x-99fa0.firebaseapp.com",
+    projectId: "project-x-99fa0",
+    storageBucket: "project-x-99fa0.appspot.com",
+    messagingSenderId: "857091344106",
+    appId: "1:857091344106:web:6bdb84070746977b9eb993",
+    measurementId: "G-BCCDQR8VEC"
+  };
